b1c2d7a1c3aea2dd989ff53d0c1bb461760f4d66
Juan Luis <juanluisariasrojas32@gmail.com>
2026-08-24 17:43:08 -0400

    fix: correct ScrollerContainer margin

--- src/components/ScrollerContainer.tsx
+++ src/components/ScrollerContainer.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 function ScrollerContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-1 mt-13 md:mt-2 overflow-y-auto h-full w-full custom-scrollbar md:rounded-t-3xl md:border border-white/35">
+    <div className="flex-1 overflow-y-auto h-full w-full custom-scrollbar md:rounded-t-3xl md:border border-white/35">
       {children}
     </div>
   );
